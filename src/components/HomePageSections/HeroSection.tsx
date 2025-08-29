7f09625452b6809366b0e5ca92f5200e77e4808c
anas-codehub <talatmahmud002025@gmail.com>
2025-08-30 00:24:07 +0600

    sadf

--- src/components/HomePageSections/HeroSection.tsx
+++ src/components/HomePageSections/HeroSection.tsx
@@ -4,11 +4,7 @@ import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
 
 const HeroSection = () => {
-  const slides = [
-    "/banner/banner1.jpg",
-    "/banner/banner2.jpg",
-    "/banner/banner3.jpg",
-  ];
+  const slides = ["eren.jpg", "eren.jpg", "eren.jpg"];
   return (
     <section>
       <Swiper
@@ -22,7 +18,7 @@ const HeroSection = () => {
         {slides.map((src, index) => (
           <SwiperSlide key={index} className="relative w-full aspect-[2/1]">
             <Image
-              src="/LCA.jpg"
+              src="/eren.jpg"
               alt={`Slide ${index + 1}`}
               fill
               className="object-cover"
